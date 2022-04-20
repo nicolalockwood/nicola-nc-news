f8a3a0a47ada26359bc23e235022e8327a92d7a0
nicolalockwood <nicolahelenlockwood@gamil.com>
2022-04-20 17:16:39 +0100

    7-comments-byID

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { getArticleCommentsByID } from '../utils/api';
 const Comments = (article_id) => {
 	const [comments, setComments] = useState([]);
+
 	useEffect(() => {
 		getArticleCommentsByID(article_id.article_id).then(({ commentData }) => {
 			setComments(commentData);
@@ -10,6 +11,18 @@ const Comments = (article_id) => {
 
 	return (
 		<main>
+			<ul className='comment_list'>
+				{comments.map((comment) => {
+					return (
+						<li className='comment_cards' key={comment.comment_id}>
+							<h2>{comment.author}</h2>
+							<p>{comment.body}</p>
+							<p>Votes:{comment.votes}</p>
+							<p>Created at:{comment.created_at}</p>
+						</li>
+					);
+				})}
+			</ul>
 			<p>Coments here</p>
 		</main>
 	);
